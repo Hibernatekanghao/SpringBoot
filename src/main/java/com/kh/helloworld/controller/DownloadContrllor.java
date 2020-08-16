7cbdb4778a9d0eb71defb5b5775ea513b2f3c3be
Hibernatekanghao <18736036089@163.com>
2020-08-16 23:49:01 +0800

    fitst commit

---
+++ src/main/java/com/kh/helloworld/controller/DownloadContrllor.java
@@ -0,0 +1,41 @@
+package com.kh.helloworld.controller;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+
+
+@Controller
+public class DownloadContrllor {
+    @GetMapping("xiazai/{filename}")
+    public ResponseEntity<byte[]> download(@PathVariable String filename) throws IOException {
+        //下载文件的路径(这里绝对路径)
+        String filepath= "D:/xiazai/"+filename;
+        File file =new File(filepath);
+        //创建字节输入流，这里不实用Buffer类
+        InputStream in = new FileInputStream(file);
+        //available:获取输入流所读取的文件的最大字节数
+        byte[] body = new byte[in.available()];
+        //把字节读取到数组中
+        in.read(body);
+        //设置请求头
+        MultiValueMap<String, String> headers = new HttpHeaders();
+        headers.add("Content-Disposition", "attchement;filename=" + file.getName());
+        //设置响应状态
+        HttpStatus statusCode = HttpStatus.OK;
+        in.close();
+        ResponseEntity<byte[]> entity = new ResponseEntity<byte[]>(body, headers, statusCode);
+        return entity;//返回
+    }
+
+}
